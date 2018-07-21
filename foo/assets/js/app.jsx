9cd1f089ad1691e573c8d17a22ea9b85d010e8ca
Anton K <lornkar@mail.ru>
2018-07-21 18:56:09 +0300

    Complit version

--- foo/assets/js/app.jsx
+++ foo/assets/js/app.jsx
@@ -1,10 +1,48 @@
-class Hello extends React.Component {
+
+class PersonItem extends React.Component {
   render() {
-    let who = "World";
     return (
-      <h1> Hello {who}! </h1>
+      <tr>
+        <td> {this.props.id}    </td>
+        <td> {this.props.first} </td>
+        <td> {this.props.last}  </td>
+      </tr>
     );
   }
 }
 
-ReactDOM.render( <Hello/>, document.querySelector("#root"));
+class PeopleList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { people: [] };
+  }
+
+  componentDidMount() {
+    this.serverRequest =
+      axios
+        .get("/people")
+        .then((result) => {
+           this.setState({ people: result.data });
+        });
+  }
+
+  render() {
+    const people = this.state.people.map((person, i) => {
+      return (
+        <PersonItem key={i} id={person.Id} first={person.First} last={person.Last} />
+      );
+    });
+
+    return (
+      <div>
+        <table><tbody>
+          <tr><th>Id</th><th>First</th><th>Last</th></tr>
+          {people}
+        </tbody></table>
+
+      </div>
+    );
+  }
+}
+
+ReactDOM.render( <PeopleList/>, document.querySelector("#root"));
