f5511a4e1f5cc7e5eaf3610ca2b82d9b1501c222
Anton K <lornkar@mail.ru>
2018-07-21 15:57:48 +0300

    add model

---
+++ foo/model/model.go
@@ -0,0 +1,14 @@
+package model
+
+type Model struct {
+	db
+}
+
+func New(db db) *Model {
+	return &Model{
+		db: db,
+	}
+}
+func (m *Model) People() ([]*Person, error) {
+	return m.SelectPeople()
+}
