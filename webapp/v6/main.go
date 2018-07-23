15a1863f11a1c1de2c95f6576a356bf8064e1d36
Anton K <lornkar@mail.ru>
2018-07-23 19:14:45 +0300

    1

--- webapp/v6/main.go
+++ webapp/v6/main.go
@@ -14,14 +14,6 @@ import (
 	//"strings"
 )
 
-var (
-	// компилируем шаблоны, если не удалось, то выходим
-	first_template = template.Must(template.ParseFiles(path.Join("templates", "index.html"), path.Join("templates", "main.html")))
-	post_template  = template.Must(template.ParseFiles(path.Join("templates", "index.html"), path.Join("templates", "book.html")))
-	read_template  = template.Must(template.ParseFiles(path.Join("templates", "index.html"), path.Join("templates", "reading.html")))
-	error_template = template.Must(template.ParseFiles(path.Join("templates", "index.html"), path.Join("templates", "error.html")))
-)
-
 func main() {
 	// для отдачи сервером статичных файлов из папки public/static
 	fs := http.FileServer(http.Dir("./public/static"))
