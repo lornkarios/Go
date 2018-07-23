c8fb9f569bd61545a5825fded50b74b0736cd3e0
Anton K <lornkar@mail.ru>
2018-07-23 19:59:50 +0300

    1

--- webapp/v6/source/parser/parser.go
+++ webapp/v6/source/parser/parser.go
@@ -23,7 +23,7 @@ type Book struct {
 	Code       string
 }
 
-func Load(md string, pNum int) (Book, int, error) {
+func Load(md string, pNum int) (Book, int64, error) {
 	info, err := os.Stat(md)
 	if err != nil {
 		if os.IsNotExist(err) {
