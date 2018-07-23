91164f99c9d09daa84e930c73e37db1b1d39e72f
Anton K <lornkar@mail.ru>
2018-07-23 18:51:50 +0300

    upgrade code struct

---
+++ webapp/v6/source/daemon/readHandler.go
@@ -0,0 +1,34 @@
+package daemon
+
+func readHandler(w http.ResponseWriter, r *http.Request) {
+	params := r.URL.Query()
+	// Извлекаем параметр
+	// Например, в http://127.0.0.1:3000/p1 page = "p1"
+	// в http://127.0.0.1:3000/ page = ""
+	page := params.Get(":page")
+	// Путь к файлу (без расширения)
+	// Например, posts/p1
+	p := path.Join("books", page)
+	var post_md string
+	if page != "" {
+		// если page не пусто, то считаем, что запрашивается файл
+		// получим posts/p1.md
+		post_md = p + ".fb2"
+	} else {
+		// если page пусто, то выдаем главную
+		if err := first_template.ExecuteTemplate(w, "layout", nil); err != nil {
+			log.Println(err.Error())
+			errorHandler(w, r, 500)
+		}
+		return
+	}
+	post, status, err := parser.Load(post_md)
+	if err != nil {
+		errorHandler(w, r, status)
+		return
+	}
+	if err := read_template.ExecuteTemplate(w, "layout", post); err != nil {
+		log.Println(err.Error())
+		errorHandler(w, r, 500)
+	}
+}
